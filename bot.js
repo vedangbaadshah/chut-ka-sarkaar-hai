60f16d718c5a31d167f9f52f5da4a60c7a196ed2
vedangbaadshah <57860080+vedangbaadshah@users.noreply.github.com>
2020-03-09 18:16:49 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+var Discord = require('discord.js');
+var botName = require('./package.json');
+var bot = new Discord.Client();
+
+bot.on('message', message =>{
+
+ // Variables
+    var sender = message.author; 
+    var msg = message.content;
+    var prefix = "," 
+    
+     if(message.author.id != "683723648753991680" && message.channel.id === "624994517111537695"){
+        if(msg.startsWith(',', 0)){
+            if(msg === prefix + "hlo" && message.channel.id === "624994517111537695"){
+                message.channel.send('AA GAYE GAANDU YAHA MARWANE KE LIYE ') // Sends a message to the channel, with the content
+            }
+            else{
+                message.channel.send("Hey, I only understand ,Hii right now. jab khali hunga :) ")
+            }
+        }
+        else{
+            message.delete();
+            message.channel.send(sender + " this bot only accepts commands which starts with '>' dot.");
+        }
+    }
+});
+
+bot.login(process.env.BOT_TOKEN)
